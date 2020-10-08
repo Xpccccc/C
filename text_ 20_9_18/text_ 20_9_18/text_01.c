a42eac44de86a89398e5d1880be1ba7d3405cf19
Xpzt <1014647664@qq.com>
2020-10-08 13:48:06 +0800

    提交代码

---
+++ text_ 20_9_18/text_ 20_9_18/text_01.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+//main前面的int表示函数调用返回一个整型值
+int main()//主函数-程序的入口
+{
+	double d = 3.14;
+	printf("%d\n",sizeof(char));
+	printf("%d\n",sizeof(int));
+	printf("%d\n",sizeof(short));
+	printf("%d\n",sizeof(double));
+	printf("%d\n",sizeof(long));
+	printf("%d\n",sizeof(long long));
+	return 0;//返回 0，与 int 配套
+}
