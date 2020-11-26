c1b4664b85adc31de89815efbbd792b3f0ef27fd
Xpzt <1014647664@qq.com>
2020-11-26 16:40:51 +0800

    动态内存分配 -- calloc的用法

---
+++ test_11_25_1/test_11_25_1/test.c
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+	int *p = (int *)calloc(10, sizeof(int));
+	if (p == NULL)
+	{
+		printf("%s\n", strerror(errno));
+	}
+	else
+	{
+		int i = 0;
+		for (i = 0; i < 10; i++)
+		{
+			printf("%d ", *(p + i));
+		}
+	}
+	free(p);
+	p = NULL;
+	return 0;
+}
