e3ab78650d893db0303620822f4ccbf662cf4d35
Xpzt <1014647664@qq.com>
2020-12-02 15:18:57 +0800

    C语言预处理 -- 避免头文件重复引入

---
+++ test_12_2_1/test_12_2_1/add.h
@@ -0,0 +1,9 @@
+
+
+//避免头文件重复引入
+#ifndef __TEST_H__
+#define __TEST_H__
+
+//#pragma once
+int Add(int x, int y);
+#endif
---
+++ test_12_2_1/test_12_2_1/test.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+#include "add.h"
+#include "add.h"
+#include "add.h"
+
+int main()
+{
+	int a = 10;
+	int b = 20;
+	int ret = Add(a, b);
+	printf("%d ", ret);
+	return 0;
+}
+
